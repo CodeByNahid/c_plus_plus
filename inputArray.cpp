97a8a43c86aedb93511c4aa352c3cceda2fae671
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-01-03 23:54:10 +0600

    inputArray.cpp

---
+++ inputArray.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int n; 
+cout<<"Enter the array size: "<<endl;
+cin>>n;
+
+int array[n];
+
+ for(int i=0;i<n;i++)cin>>array[i];
+ 
+ cout<<"Inputed Array:"<<endl;
+ for(int i=0;i<n;i++)cout<<array[i]<<" ";
+}
