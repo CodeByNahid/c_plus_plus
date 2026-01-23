a0482c68908fcd798aa1a3fdede62aa2f28fa5f5
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-01-23 23:00:01 +0600

    Create BREAKSTICK.cpp

---
+++ BREAKSTICK.cpp
@@ -0,0 +1,15 @@
+//Problem Link: https://www.codechef.com/problems/BREAKSTICK
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;cin>>t; while (t--){
+    int x,y;
+    cin>>x>>y;
+     if(y%2==0 && (x%2)!=0)cout<<"NO"<<endl;
+    else {
+        cout<<"YES"<<endl;
+    }
+}
+
+}
