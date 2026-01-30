b2f2b89b992781cb3f55727df3ecdbbaffbb457c
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-01-30 21:23:50 +0600

    Create ADD12GAME.cpp

---
+++ ADD12GAME.cpp
@@ -0,0 +1,21 @@
+//Problem Link: https://www.codechef.com/problems/ADD12GAME
+#include <bits/stdc++.h>
+using namespace std;
+int sl(){
+    int n; cin>>n;
+    if(n==1){
+        cout<<"ALICE"<<endl;
+       
+    }
+    else{
+      cout<<"BOB"<<endl; 
+      
+    }
+     return 0;
+}
+int main() {
+	int t; cin>>t; while(t--){
+	    sl();
+	}
+
+}
