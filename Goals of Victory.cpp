cc176c8d1cdf13c987f961af8269d2cc37417a84
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-01-31 22:45:15 +0600

    Create Goals of Victory.cpp

---
+++ Goals of Victory.cpp
@@ -0,0 +1,16 @@
+//Problem Link: https://codeforces.com/contest/1877/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t; 
+	while(t--){
+	    int n; cin>>n;
+	    n--;
+	    int ans=0,x; 
+	    while(n--){cin>>x; ans+=x;}
+	    cout<<ans*-1<<endl;
+	    
+	}
+
+}
