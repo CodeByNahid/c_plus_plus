901234a55eb49db8e254b33e6d90b89a671d1435
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-02-21 22:11:37 +0600

    Create EASYPERM.cpp

---
+++ EASYPERM.cpp
@@ -0,0 +1,13 @@
+//Problem Link: https://www.codechef.com/problems/EASYPERM
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while (t--){
+	    int n; cin>>n;
+	    for(int i=n;i>=1;i--) cout<<i<<" ";	
+	    cout<<endl;
+	}
+
+}
