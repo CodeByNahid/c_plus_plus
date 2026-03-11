87619e4b35fab75a7559732c22351d88ecdb5708
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-03-11 22:38:18 +0600

    Create VACAEX.cpp

---
+++ VACAEX.cpp
@@ -0,0 +1,18 @@
+//Problem Link: https://www.codechef.com/problems/VACAEX
+//@CodeByNahid
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int x,y; cin>>x>>y;
+    cout<<x+y-1<<endl; return;
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t=1; while (t--) {
+        solve();
+    }
+return 0;
+}
