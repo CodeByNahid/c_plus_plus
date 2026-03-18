6ea591843de7439b0b4cf5d42963f964591357c1
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-03-18 23:07:45 +0600

    Create Vus the Cossack and a Contest.cpp

---
+++ Vus the Cossack and a Contest.cpp
@@ -0,0 +1,18 @@
+//Problem Link: https://codeforces.com/contest/1186/problem/A
+//@CodeByNahid
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,x,y; cin>>n>>x>>y;
+    cout<<(x>=n && y>=n?"Yes":"No")<<endl; return;
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t=1; while (t--) {
+        solve();
+    }
+return 0;
+}
