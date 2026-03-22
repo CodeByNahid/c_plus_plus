514e165e327afa92d34e0f74d123846da22afb1b
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-03-22 17:39:01 +0600

    Add files via upload

---
+++ COVID_19.cpp
@@ -0,0 +1,18 @@
+//problem Link: https://www.codechef.com/problems/COVID_19
+//@CodeByNahid
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,m; cin>>n>>m;
+    cout<<((n+1)/2)*((m+1)/2)<<endl;
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t; cin >> t; while (t--) {
+        solve();
+    }
+return 0;
+}
