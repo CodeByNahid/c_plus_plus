d77f77f63ecd3e79b2fcabf1c9cddb5b7b0dd3e0
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-04-12 22:34:47 +0600

    Create try12apri.cpp

---
+++ try12apri.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        int even = 0, odd = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            if(a[i] % 2 != i%2)
+            {
+                if(i%2==0)
+                {
+                    even++;
+                }
+                else
+                    odd++;
+            }
+
+
+        }
+
+        if(even!=odd)
+        {
+            cout << "-1" << endl;
+        }
+
+        else
+             cout <<even<< endl;
+        }
+    return 0;
+}
+
