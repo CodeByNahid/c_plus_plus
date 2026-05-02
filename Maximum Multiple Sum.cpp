391ba621eed02d2ebecec858e44a544b831e4f3b
MD. Nahid Hasan <nahid.coding@gmail.com>
2026-05-02 23:27:14 +0600

    Create Maximum Multiple Sum.cpp

---
+++ Maximum Multiple Sum.cpp
@@ -0,0 +1,18 @@
+//Problem Link:https://codeforces.com/contest/1985/problem/B
+//@CodeByNahid
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    cout<<(n==3?3:2)<<endl;
+}
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t; cin >> t; while (t--) {
+        solve();
+    }
+return 0;
+}
